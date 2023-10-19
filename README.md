6191803cffe1ce8aad26e2ff39a46c627571f03e
AmirHossein Sakhrawi <63424665+AmirSa12@users.noreply.github.com>
2023-10-19 09:24:37 +0330

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# store
+
+- [ ] Login/Sign up / logout
+- [ ] Cart UI
+- [ ] Product Card
+- [ ] create neon db
+- [ ] plugin@auth (login/signup/logout)
+- [ ] verify email (this may not work because of edge, nodemailer)
+- [ ] User schema
+- [ ] Product schema
+- [ ] Get all products
+- [ ] create product
+- [ ] update product (price, quantity)
+- [ ] remove product
+- [ ] upload image (optional)
+- [ ] stripe (later)
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import UnoCSS from 'unocss/vite'
+
+export default defineConfig(() => {
+  return {
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), UnoCSS()],
+    preview: {
+      headers: {
+        "Cache-Control": "public, max-age=600",
+      },
+    },
+  };
+});
