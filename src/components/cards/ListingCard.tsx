69fe7cc90c5c6d40d10efc8283f3b52b3b2c2c3b
jawad-sawari <javad.sbf@gmail.com>
2023-10-19 13:19:50 +0330

    listing route

--- src/components/cards/ListingCard.tsx
+++ src/components/cards/ListingCard.tsx
@@ -1,14 +1,17 @@
 import { component$ } from "@builder.io/qwik";
 
 interface ProductProps {
-  name: string;
+  title: string;
 }
 
-const ListingCard = component$<ProductProps>(({ name }) => {
+const ListingCard = component$<ProductProps>(({ title }) => {
   return (
-    <div class="flex flex-col items-center justify-center w-100px h-100px border rounded-8px bg-white">
-      <p class="text-20px font-500 text-black my-0">{name}</p>
-    </div>
+    <a
+      href="/listing"
+      class="flex flex-col items-center justify-center w-100px h-100px border rounded-8px bg-white"
+    >
+      <p class="text-20px font-500 text-black my-0">{title}</p>
+    </a>
   );
 });
 export default ListingCard;
